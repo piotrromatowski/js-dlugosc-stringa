500475a0fb49a1e97ad81767f33f77e6c8a110e6
Piotr <piotr.romatowski@gmail.com>
2020-02-11 17:47:03 +0100

    js-dlugosc-stringa

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 let tekst1 = "Uwielbiam JavaScript!!"
 let tekst2 = "Jestem świetnym programistą"
 
-console.log(tekst1+tekst2);
+function longertext (tekst1,tekst2) {
+    if (tekst2.length>tekst1.length){
+        console.log(tekst2)}
+        else if (tekst1.length>tekst2.length){
+            console.log(tekst1)
+        }
+}
+
+longertext(tekst1,tekst2)
